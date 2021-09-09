34fcac8d3d29399a0336c7a84f4179835089f226
Tim Wise <tim@leisurehound.com>
2021-09-08 22:51:44 -0700

    Does FoundationNetworking help?

--- Sources/MQTTSession.swift
+++ Sources/MQTTSession.swift
@@ -8,6 +8,9 @@
 
 import Foundation
 import CornucopiaStreams
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
 
 final public class MQTTSession: NSObject, StreamDelegate {
     private var options: MQTTOptions
