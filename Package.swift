43c034836d338c59a38f0ac65c4c76e7618e7f31
Emory Dunn <emory@emorydunn.com>
2021-11-13 16:25:36 -0800

    Drop watchOS version

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         .macOS("10.15.4"),
         .iOS("13.4"),
         .tvOS("13.4"),
-        .watchOS("6"),
+        .watchOS(.v5)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
